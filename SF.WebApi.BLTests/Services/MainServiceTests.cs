57221f347d17741182a983b987f2475f6a13d931
Афанасьев Дмитрий <dmitriy.afanasev@info-pro.ru>
2024-03-29 20:45:54 +0800

    Начало основного сервиса

---
+++ SF.WebApi.BLTests/Services/MainServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SF.WebApi.BL.Services.Tests;
+
+[TestClass()]
+public class MainServiceTests
+{
+    [TestMethod()]
+    public void RunTest()
+    {
+        //arrange
+        MainService mainService = new MainService();
+
+        //action
+        var result = mainService.Run();
+
+        //assert
+        Assert.AreEqual(result.First(), 15);
+        Assert.AreEqual(result.Last(), 20);
+    }
+}
