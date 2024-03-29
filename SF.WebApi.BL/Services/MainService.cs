ffdaab6ff18664b2c2c2eb37e48d1da74809d5f1
Афанасьев Дмитрий <dmitriy.afanasev@info-pro.ru>
2024-03-29 20:45:54 +0800

    Начало основного сервиса

---
+++ SF.WebApi.BL/Services/MainService.cs
@@ -0,0 +1,20 @@
+﻿namespace SF.WebApi.BL.Services;
+
+public interface IMainService
+{
+    List<int> Run();
+}
+
+public class MainService : IMainService
+{
+    public List<int> Run()
+    {
+        var list = new List<int>() { 10, 15, 20 };
+        int i = 10;
+        var query = list.Where(x => x >= i);
+        i = 15;
+        var result = query.ToList();
+
+        return result;
+    }
+}
---
+++ SF.WebApi.BLTests/Services/MainServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SF.WebApi.BL.Services.Tests;
+
+[TestClass()]
+public class MainServiceTests
+{
+    [TestMethod("Основной метод, запуск")]
+    public void RunTest()
+    {
+        //arrange
+        var mainService = new MainService();
+
+        //action
+        var result = mainService.Run();
+
+        //assert
+        Assert.AreEqual(result.First(), 15);
+        Assert.AreEqual(result.Last(), 20);
+    }
+}
