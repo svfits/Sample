bd0db3ee5d5d8a90ccc4ce140b62efc8f59f5668
Афанасьев Дмитрий <dmitriy.afanasev@info-pro.ru>
2024-04-05 19:40:46 +0800

    Сортировка вставками

---
+++ SF.WebApi.BL/Services/InsertionSortService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.WebApi.BL.Services;
+
+public interface IInsertionSortService
+{
+    int[] Start(int[] numbers);
+}
+
+public class InsertionSortService : IInsertionSortService
+{
+    public int[] Start(int[] numbers)
+    {
+        int index;
+        int currentNumber;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            index = i;
+            currentNumber = numbers[i];
+
+            while (index > 0 && currentNumber < numbers[index - 1])
+            {
+                numbers[index] = numbers[index - 1];
+                index--;
+            }
+            numbers[index] = currentNumber;
+        }
+
+        return numbers;
+    }
+}
---
+++ SF.WebApi.BLTests/Services/InsertionSortServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace SF.WebApi.BL.Services.Tests;
+
+[TestClass()]
+public class InsertionSortServiceTests
+{
+    [TestMethod("Сортировка вставками")]
+    public void StartTest()
+    {
+        //arrange
+        var insertionSortService = new InsertionSortService();
+        int[] numbers = [127, 21, 211, 12, 21, 23, 312, 42456, 5, 67789, 78];
+
+        //action
+        var result = insertionSortService.Start(numbers);
+
+        //assert
+        foreach (var item in result)
+        {
+            Debug.WriteLine(item);
+        }
+    }
+}
