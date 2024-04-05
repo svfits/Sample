368a124ac2ad7508e0f11203e9135f5d8f4f499f
Афанасьев Дмитрий <dmitriy.afanasev@info-pro.ru>
2024-04-05 17:55:12 +0800

    Быстрая сортировка

---
+++ SF.WebApi.BL/Services/QuickSortService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.WebApi.BL.Services;
+
+public interface IQuickSortService
+{
+    int[] Start(int[] numbers);
+}
+
+/// <summary>
+/// Быстрая сортировка
+/// Находим опорный элемент
+/// Перемещаем те что меньше его перед ним изменяя его индекс
+/// </summary>
+public class QuickSortService : IQuickSortService
+{
+    public int[] Start(int[] numbers)
+    {
+        numbers = QuickSort(numbers, 0, numbers.Length - 1);
+
+        return numbers;
+    }
+
+    private static int FindPivot(int[] numbers, int minIndex, int maxIndex)
+    {
+        int pivot = minIndex - 1;
+        int temp;
+        for (int i = minIndex; i < maxIndex; i++)
+        {
+            if (numbers[i] < numbers[maxIndex])
+            {
+                pivot++;
+                temp = numbers[pivot];
+                numbers[pivot] = numbers[i];
+                numbers[i] = temp;
+            }
+        }
+
+        pivot++;
+        temp = numbers[pivot];
+        numbers[pivot] = numbers[maxIndex];
+        numbers[maxIndex] = temp;
+
+        return pivot;
+    }
+
+    private static int[] QuickSort(int[] numbers, int minIndex, int maxIndex)
+    {
+        if (minIndex >= maxIndex)
+        {
+            return numbers;
+        }
+
+        int pivot = FindPivot(numbers, minIndex, maxIndex);
+        QuickSort(numbers, minIndex, pivot - 1);
+        QuickSort(numbers, pivot + 1, maxIndex);
+
+        return numbers;
+    }
+}
---
+++ SF.WebApi.BLTests/Services/QuickSortServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace SF.WebApi.BL.Services.Tests;
+
+[TestClass()]
+public class QuickSortServiceTests
+{
+    [TestMethod("Быстрая сортировка")]
+    public void StartTest()
+    {
+        //arrange
+        var quickSortService = new QuickSortService();
+        int[] numbers = { 1232, 22, 23, 43, 51, 63, 7, 85, 19, 101, 1122 };
+
+        //action
+        var result = quickSortService.Start(numbers);
+
+        //assert
+        foreach (var item in result)
+        {
+            Debug.WriteLine(item);
+        }
+    }
+}
