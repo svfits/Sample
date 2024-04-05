2f87d61b2c436af6b874f3bc730d64d1ce8f8eda
Афанасьев Дмитрий <dmitriy.afanasev@info-pro.ru>
2024-04-05 12:22:51 +0800

    Добавлен сервис с задачами

---
+++ SF.WebApi.BL/Services/TasksService.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace SF.WebApi.BL.Services;
+
+public interface ITasksService
+{
+    List<int> Run();
+
+    void RunParallel();
+    Task RunSequentially();
+}
+
+public class TasksService : ITasksService
+{
+    public List<int> Run()
+    {
+        var list = new List<int>() { 10, 15, 20 };
+        int i = 10;
+        var query = list.Where(x => x >= i);
+        i = 15;
+        var result = query.ToList();
+        list.Clear();
+
+        return result;
+    }
+
+    public void RunParallel()
+    {
+        Debug.WriteLine("Запуск параллельно");
+        var list = new List<Task>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            var t = i;
+            var task = Task.Run(() => Start(t));
+            list.Add(task);
+        }
+
+        Task.WaitAll(list.ToArray());
+    }
+
+    private static async Task Start(int i)
+    {
+        Debug.WriteLine("Ждем паузы " + DateTime.Now + " " + i);
+        await Task.Delay(2000);
+    }
+
+    public async Task RunSequentially()
+    {
+        Debug.WriteLine("Запуск последовательно");
+
+        for (int i = 0; i < 10; i++)
+        {
+            await Start(i);
+        }
+    }
+}
---
+++ SF.WebApi.BLTests/Services/TasksServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SF.WebApi.BL.Services.Tests;
+
+[TestClass()]
+public class TasksServiceTests
+{
+    [TestMethod("Основной метод, запуск")]
+    public void RunTest()
+    {
+        //arrange
+        var tasksService = new TasksService();
+
+        //action
+        var result = tasksService.Run();
+
+        //assert
+        Assert.AreEqual(result.First(), 15);
+        Assert.AreEqual(result.Last(), 20);
+    }
+
+    [TestMethod("Запуск параллельно")]
+    public void RunParallel()
+    {
+        //arrange
+        var tasksService = new TasksService();
+
+        //action
+        tasksService.RunParallel();
+
+        //assert        
+    }
+
+    [TestMethod("Запуск последовательно")]
+    public async Task RunSequentiallyTest()
+    {
+        //arrange
+        var tasksService = new TasksService();
+
+        //action
+        await tasksService.RunSequentially();
+
+        //assert
+    }
+}
