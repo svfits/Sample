57f2aa2f1a0c350c2c9560a5e6495319429f62ef
Афанасьев Дмитрий <dmitriy.afanasev@info-pro.ru>
2024-04-07 23:18:57 +0800

    Работа со ссылочными типами

---
+++ SF.WebApi.BL/Services/ReferenceService.cs
@@ -0,0 +1,26 @@
+﻿namespace SF.WebApi.BL.Services;
+
+public class ReferenceService
+{
+    public int MyProperty { get; set; }
+
+    public static (string, ReferenceService) Start()
+    {
+        var mainService = new ReferenceService
+        {
+            MyProperty = 1234
+        };
+
+        string rrrr = "до изменения";
+
+        Changes(mainService, rrrr);
+
+        return (rrrr, mainService);
+    }
+
+    private static void Changes(ReferenceService mainService, string rrrr)
+    {
+        rrrr = "изменится ли?";
+        mainService.MyProperty = 99995;
+    }
+}
---
+++ SF.WebApi.BLTests/Services/ReferenceServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SF.WebApi.BL.Services.Tests;
+
+[TestClass()]
+public class ReferenceServiceTests
+{
+    [TestMethod("Поведение строк и ссылочных типов")]
+    public void StartTest()
+    {
+        //arrange
+        
+
+        //action
+        (string strResult, ReferenceService obj) = ReferenceService.Start();
+
+        //assert
+        Assert.AreEqual(strResult, "до изменения");
+        Assert.AreEqual(obj.MyProperty, 99995);
+    }
+}
