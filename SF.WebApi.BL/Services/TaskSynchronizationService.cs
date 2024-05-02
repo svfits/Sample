d1e0cd67c2643aa7bfb1a962d7e8e3e3ce80b7c3
Афанасьев Дмитрий <dmitriy.afanasev@info-pro.ru>
2024-05-02 18:26:13 +0800

    Добавлена кафка без настроек

---
+++ SF.WebApi.BL/Services/TaskSynchronizationService.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace SF.WebApi.BL.Services;
+
+public class TaskSynchronizationService
+{
+    public static int MyProperty = 1;
+
+    private static readonly SemaphoreSlim semaphor = new(1, 1);
+
+    private static async Task StartIteration(int i)
+    {
+        //await semaphor.WaitAsync();
+
+        Debug.WriteLine(i);
+        await Task.Delay(500);
+        MyProperty *= i;
+
+        //semaphor.Release(); 
+    }
+
+    public static void Run()
+    {
+        var list = new List<Task>();
+        for (int i = 1; i < 10; i++)
+        {
+            var t = i;
+            var task = Task.Run(() => StartIteration(t));
+            list.Add(task);
+        }
+
+        Task.WaitAll(list.ToArray());
+    }
+}
---
+++ SF.WebApi.BLTests/Services/TaskSynchronizationServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace SF.WebApi.BL.Services.Tests;
+
+[TestClass()]
+public class TaskSynchronizationServiceTests
+{
+    [TestMethod()]
+    public void RunTest()
+    {
+        //arrange
+
+        //action
+        TaskSynchronizationService.Run();
+
+        //assert
+        Debug.WriteLine("Ответ: " + TaskSynchronizationService.MyProperty);      
+    }
+}
