1a26e42a2b6538e8de18a1943ecd75c9f653e288
Афанасьев Дмитрий <dafanasev@almasc.ru>
2024-08-11 23:12:27 +0800

    Пример для action

---
+++ SF.WebApi.BL/Services/LamdaService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.WebApi.BL.Services;
+
+public interface ILambdaService
+{
+    void RunAction();
+}
+
+public class LambdaService : ILambdaService
+{
+    public void RunAction()
+    {
+        Start(a =>
+        {
+            a.MyProperty = 8;
+            a.MyProperty2 = 7;
+            a.MyProperty3 = 6;
+            a.Method(234);
+        });
+    }
+
+    /// <summary>
+    /// Тут произойдет заполнение объекта myClass
+    /// </summary>
+    /// <param name="action"></param>
+    private static void Start(Action<MyClass> action)
+    {
+        var myClass = new MyClass();
+        action(myClass);
+    }
+}
+
+public class MyClass
+{
+    public int MyProperty { get; set; }
+
+    public int MyProperty2 { get; set; }
+
+    public int MyProperty3 { get; set; }
+
+    public void Method(int v)
+    {
+        Debug.WriteLine(v);
+    }
+}
---
+++ SF.WebApi.BLTests/Services/LambdaServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SF.WebApi.BL.Services.Tests;
+
+[TestClass()]
+public class LambdaServiceTests
+{
+    [TestMethod()]
+    public void RunTest()
+    {
+        //arrange
+        var lambdaService = new LambdaService();
+
+        //action
+        lambdaService.RunAction();
+
+        //assert        
+    }
+}
